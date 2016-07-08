98319ee1c1e4ea04174b8ea0b5e9fb2a0a957bef
Vinay Maneti <vinaymaneti@gmail.com>
2016-07-08 23:22:01 +0700

    +Added contacts us page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'welcome/index'
+  get 'contact_us' => 'welcome#contact_us'
 
   root 'welcome#index'
 end
