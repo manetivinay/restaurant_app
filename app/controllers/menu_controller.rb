3a023eb3ab48c5dd9092f7d71b2686bf201311c3
Vinay Maneti <vinaymaneti@gmail.com>
2016-07-10 16:06:50 +0700

    +Added increase teh view in Menu controller

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -2,5 +2,6 @@ class MenuController < ApplicationController
   def index
     @sections = %w(Lunch Drinks Cookies Breakfast Dinner)
     @food_items = FoodItem.by_section(params[:section])
+    @food_item.add_view
   end
 end
