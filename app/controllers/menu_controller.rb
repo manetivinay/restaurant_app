89a45a1af14e91703b8df13812ed90baf29ef322
Vinay Maneti <vinaymaneti@gmail.com>
2016-07-10 11:57:26 +0700

    +Added more 2 section in food item -- Breakfast and Dinner

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,6 +1,6 @@
 class MenuController < ApplicationController
   def index
-    @sections = %w(Lunch Drinks Cookies)
+    @sections = %w(Lunch Drinks Cookies Breakfast Dinner)
     @food_items = FoodItem.by_section(params[:section])
   end
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,5 +1,5 @@
 class Section
   def self.all
-    %w(Lunch Drinks Cookies)
+    %w(Lunch Drinks Cookies Breakfast Dinner)
   end
 end
